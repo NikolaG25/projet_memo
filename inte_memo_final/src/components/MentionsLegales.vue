113fd0574de786f197a3e2ff5f26e46343d92c21
Charles Mangin <charles.mangin@edu.univ-fcomte.fr>
2022-01-02 14:19:44 +0100

    Page mentions légales

--- inte_memo_final/src/components/MentionsLegales.vue
+++ inte_memo_final/src/components/MentionsLegales.vue
@@ -1,6 +1,76 @@
 <template>
-  <div>
-    <p>Mentions Légales</p>
+  <div id="mentions_legales">
+    <div>
+      <h1>Mentions légales</h1>
+    </div>
+
+    <div>
+      <p>Le site www.asso-memo.fr propose des ressources documentaires sur la vie étudiante et le paysage associatif de la région de Montbéliard, des guides pratiques sur les bons plans de la région, ainsi qu'un forum d'échange entre étudiant. Le site donne aussi la possibilité aux utilisateurs de devenir des adhérents de l'association via l'utilisation d'un site tier : <a href="www.helloasso.com">www.helloasso.com</a>.</p>
+    </div>
+    <div>
+      <h2 class="title_underline">Informations éditoriales</h2>
+      <p><strong>Éditeur</strong></p>
+      <p>Association MéMO (Maison des étudiants de Montbéliard)<span class="span-block"></span>
+        Place Lucien Tharradin<span class="span-block"></span>
+        25 200 Montbéliard<span class="span-block"></span>
+        Contact: association.memo@gmail.com</p>
+      <h2 class="title_underline">Protection et traitement de données à caractère personnel</h2>
+      <p>
+        <strong>Respect des lois en vigueur</strong>
+        <span class="span-block"></span>
+        Le site www.asso-memo.fr respecte la vie privée de l’internaute et se conforme strictement aux lois en vigueur sur la protection de la vie privée et des libertés individuelles. Aucune information personnelle n’est collectée à votre insu. Aucune information personnelle n’est cédée à des tiers. Les courriels, les adresses électroniques ou autres informations nominatives dont ce site est destinataire ne font l’objet d’aucune exploitation et ne sont conservés que pour la durée nécessaire à leur traitement.</p>
+
+        <span class="span-block"></span>
+        <strong>Cookies</strong>
+        <span class="span-block"></span>
+        Lors de la consultation de notre site asso-memo.fr, des cookies sont déposés sur votre ordinateur, votre mobile ou votre tablette.<span class="span-block"></span>
+      <p>  Les seuls cookies utilisés par le site  sont ceux destinés à la mesure d’audience et ne collectent pas de données personnelles. Les outils de mesures d’audience sont déployés afin d’obtenir des informations sur la navigation des visiteurs. Ils permettent notamment de comprendre comment les utilisateurs arrivent sur un site et de reconstituer leur parcours.</p>
+      <p>  Les données générées par les cookies sont transmises et stockées par les prestataires de mesure d’audience (Xiti). Les prestataires de mesure d’audience sont susceptibles de communiquer ces données à des tiers en cas d’obligation légale ou lorsque ces tiers traitent ces données pour leur compte.</p>
+      <ul>
+        <li>
+          <strong>Définition d’un cookie</strong>
+          <span class="span-block"></span>
+        Un cookie est un fichier texte déposé sur votre ordinateur lors de la visite d’un site ou de la consultation d’une publicité. Il a pour but de collecter des informations relatives à votre navigation et de vous adresser des services adaptés à votre terminal (ordinateur, mobile ou tablette). Les cookies sont gérés par votre navigateur internet.<span class="span-block"></span>
+        Nous veillons dans la mesure du possible à ce que les prestataires de mesures d’audience respectent strictement la loi informatique et libertés du 6 janvier 1978 modifiée et s’engagent à mettre en œuvre des mesures appropriées de sécurisation et de protection de la confidentialité des données.</li>
+      </ul>
+      <ul>
+        <li>
+          <strong>Paramétrer votre navigateur internet</strong>
+          <span class="span-block"></span>
+        Vous pouvez à tout moment choisir de désactiver ces cookies. Votre navigateur peut également être paramétré pour vous signaler les cookies qui sont déposés dans votre ordinateur et vous demander de les accepter ou pas. Vous pouvez accepter ou refuser les cookies au cas par cas ou bien les refuser systématiquement.<span class="span-block"></span>
+        En cas de refus de depôt de cookie, vous ne pourrez pas consulter le site asso-memo.fr. Afin de gérer les cookies au plus près de vos attentes nous vous invitons à paramétrer votre navigateur en tenant compte de la finalité des cookies.</li>
+      </ul>
+
+      <h2 class="title_underline">Prestataire d’hébergement</h2>
+      <p>
+        <strong>OVH</strong>
+        <span class="span-block"></span>
+        Siège Social<span class="span-block"></span>
+        1801 McGill College Ave., Suite 800<span class="span-block"></span>
+        Montreal, Quebec H3A 2N4, Canada<span class="span-block"></span>
+        <a href="https://www.ovhcloud.com" target="_blank">www.ovhcloud.com</a>
+      </p>
+
+      <h2 class="title_underline">Droits d’auteur et droits de reproduction</h2>
+      <p>
+        L'Association MéMO (Maison des étudiants de Montbéliard) conserve l'intégralité des droits d'auteur relatifs au contenu des textes et images graphiques détenus par L'Association MéMO (Maison des étudiants de Montbéliard) et vous autorise par les présentes à copier par voie électronique les documents publiés ici aux seules fins de communication ou de consultation des informations. Vous n'êtes pas autorisé(e) à reproduire, modifier ou altérer de quelque manière que ce soit les fichiers de ce site Web en vue de rediffuser ou d'imprimer les informations que celui-ci contient sans l'autorisation écrite de L'Association MéMO (Maison des étudiants de Montbéliard). Sauf dans les cas expressément indiqués ci-dessus, aucune disposition des présentes ne sera interprétée comme conférant une licence ou un droit en vertu d'un droit d'auteur, d'un brevet ou d'une marque commerciale L'Association MéMO (Maison des étudiants de Montbéliard).
+      <p>
+        <strong>Crédits photos et illustrations</strong>  <span class="span-block"></span>
+        Toutes les photographies du site sont propriété de L'Association MéMO (Maison des étudiants de Montbéliard) ou de leurs auteurs et sont utilisées avec leur aimable autorisation.</p>
+      <p>
+        <strong>Création de liens vers le site</strong>
+        <span class="span-block"></span>
+        Le site asso-memo.fr autorise, sans autorisation préalable, la mise en place de liens hypertextes pointant vers ses pages, sous réserve de&nbsp;:</p>
+      <ul class="spip">
+        <li> ne pas utiliser la technique du lien profond, c’est-à-dire que les pages du site asso-memo.fr ne doivent pas être imbriquées à l’intérieur des pages d’un autre site, mais visibles par l’ouverture d’une fenêtre indépendante.</li>
+        <li> mentionner la source qui pointera grâce à un lien hypertexte directement sur le contenu visé.</li>
+        <li> ne pas utiliser le logotype de l'association sans autorisation</li>
+      </ul>
+      <p>Les sites qui font le choix de pointer vers asso-memo.fr engagent leur responsabilité dès lors qu’ils porteraient atteinte à l’image du site.</p>
+      <h2 class="title_underline">Responsabilité face aux liens vers d’autres sites</h2>
+      <p>Le site asso-memo.fr propose des liens vers d’autres sites, essentiellement des sites de partenaires.<span class="span-block"></span>
+        Ces pages ne font pas partie du site asso-memo.fr&nbsp;: elles n’engagent pas la responsabilité de la rédaction du site qui ne peut être tenue pour responsable des contenus vers lesquels ces liens pointent. Ils ne visent qu’à permettre à l’internaute d’accéder plus facilement à d’autres ressources documentaires sur le sujet consulté.</p>
+    </div>
   </div>
 </template>
 
